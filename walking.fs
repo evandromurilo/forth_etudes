6c56b79a0dd9439fef13bfc42a75bec42f9ee81c
Evandro Murilo <evandromurilob@hotmail.com>
2020-05-13 21:48:34 -0400

    clean up and add instructions to walking.fs

--- walking.fs
+++ walking.fs
@@ -1,7 +1,19 @@
+( helper words )
+
+: 4DUP ( a b c d -- a b c d a b c d )
+    >R >R 2DUP I I' 2SWAP R> R> ;
+
+: MATCH-COORDINATES ( ax ay bx by -- bool )
+    ROT = ROT ROT = AND ;
+
+( game constants )
+
 : MAX-Y 20 ;
 : MIN-Y 1 ;
 : MAX-X 20 ;
 : MIN-X 1 ;
+
+( player movement checking )
 
 : CAN-MOVE-DOWN ( x y -- bool )
     DUP MAX-Y < ;
@@ -12,44 +24,41 @@
 : CAN-MOVE-RIGHT ( x y -- bool )
     OVER MAX-X < ;
 
-: SKIP-LINES 0 DO CR LOOP ;
-: SKIP-COLS 0 DO 32 EMIT LOOP ;
+( rendering and game logic)
 
-: 4DUP ( a b c d -- a b c d a b c d )
-    >R >R 2DUP I I' 2SWAP R> R> ;
-
-: MATCH-COORDINATES ( ax ay bx by -- bool )
-    ROT = ROT ROT = AND ;
+: INSTRUCTIONS
+    ." UTILIZE J (BAIXO) K (CIMA) L (DIREITA) H (ESQUERDA) PARA SE MOVER "
+    CR ;
 
 : RENDER-COORDINATE ( fx fy px py cx cy )
     4DUP MATCH-COORDINATES IF 35 EMIT DROP DROP ELSE
          >R >R 2OVER R> R> MATCH-COORDINATES IF 42 EMIT ELSE
 	 32 EMIT
     THEN THEN ;
-
 : RENDER-LINE ( fx fy px py ly )
     >R MAX-X MIN-X DO I J RENDER-COORDINATE LOOP R> DROP CR ;
-
 : RENDER ( fx fy px py )
     PAGE MAX-Y MIN-Y DO I RENDER-LINE LOOP ;
-
 : CHECK-WIN ( fx fy px py )
     4DUP MATCH-COORDINATES ;
+: GAME-LOGIC ( fx fy px py )
+    RENDER CHECK-WIN IF ." VOCÊ VENCEU!! " 2DROP 2DROP QUIT ELSE INSTRUCTIONS THEN ;
 
-: GAME-LOOP ( fx fy px py )
-    RENDER CHECK-WIN IF ." VOCÊ VENCEU!! " 2DROP 2DROP THEN ;
+( player movement )
+
+: DOWN CAN-MOVE-DOWN IF 1 + THEN GAME-LOGIC ;
+: J DOWN ;
+: UP CAN-MOVE-UP IF 1 - THEN GAME-LOGIC ;
+: K UP ;
+: LEFT CAN-MOVE-LEFT IF SWAP 1 - SWAP THEN GAME-LOGIC ;
+: H LEFT ;
+: RIGHT CAN-MOVE-RIGHT IF SWAP 1 + SWAP THEN GAME-LOGIC ;
+: L RIGHT ;
+
+( startup ) 
 
 : INIT-FOOD 5 12 ;
 : INIT-PLAYER 1 1 ;
-: BEGIN INIT-FOOD INIT-PLAYER RENDER ;
-
-: DOWN CAN-MOVE-DOWN IF 1 + THEN GAME-LOOP ;
-: D DOWN ;
-: UP CAN-MOVE-UP IF 1 - THEN GAME-LOOP ;
-: U UP ;
-: LEFT CAN-MOVE-LEFT IF SWAP 1 - SWAP THEN GAME-LOOP ;
-: L LEFT ;
-: RIGHT CAN-MOVE-RIGHT IF SWAP 1 + SWAP THEN GAME-LOOP ;
-: R RIGHT ;
+: BEGIN INIT-FOOD INIT-PLAYER GAME-LOGIC ;
 
 BEGIN
